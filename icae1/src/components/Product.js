59fc403462295df9b5f4c8bf91c090a1009f28df
Thilina Lakshitha <lakshithathilina74@gmail.com>
2024-12-03 23:07:32 +0530

    Update Product.js page

--- icae1/src/components/Product.js
+++ icae1/src/components/Product.js
@@ -1,19 +1,28 @@
 import '../assets/CSS/layout.css';
-export default function Product({flower}){
+import React, { useState } from 'react';
+export default function Product({ flower, addToCart}){
+    
+    const [quantity, setQuantity] = useState(0);
+
+    const handleAddToCart = () => {
+        if (quantity > 0) {
+            addToCart(flower, quantity);
+            setQuantity(0); 
+        }
+    };
     
     return(
-        
         <div className="grid-item">
 
             <div class="card">
-                <img  src={require(`../assets/image/${flower.img}`)} alt={flower.name}/>
+            <img src={require('../assets/image/'+flower.img)} alt={flower.name} />
                 <div class="card-body">
                     <h5 class="card-title">Price:{flower.price}</h5>
                     <div class="quantity-container">
                         <label for="quantity">Quantity:</label>
-                        <input type="number" id="quantity" name="quantity" />
+                        <input type="number" id="quantity" name="quantity" value={quantity} onChange={(e) => setQuantity(Number(e.target.value))}/>
                     </div>
-                    <button class="card-button">Add to Cart</button>
+                    <button class="card-button" onClick={handleAddToCart}>Add to Cart</button>
                 </div>
             </div>
         </div>
