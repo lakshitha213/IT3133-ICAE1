fae877254a2159a92d402089030c3a1327dba936
Thilina Lakshitha <lakshithathilina74@gmail.com>
2024-12-03 22:41:11 +0530

    Update product.js page

--- icae1/src/components/Product.js
+++ icae1/src/components/Product.js
@@ -1,13 +1,14 @@
 import '../assets/CSS/layout.css';
-export default function Product(){
+export default function Product({flower}){
     
     return(
+        
         <div className="grid-item">
 
             <div class="card">
-                <img  />
+                <img  src={require(`../assets/image/${flower.img}`)} alt={flower.name}/>
                 <div class="card-body">
-                    <h5 class="card-title">Price:</h5>
+                    <h5 class="card-title">Price:{flower.price}</h5>
                     <div class="quantity-container">
                         <label for="quantity">Quantity:</label>
                         <input type="number" id="quantity" name="quantity" />
